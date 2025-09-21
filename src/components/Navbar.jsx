b56db93688010c7a69146a935f2e09e940665f40
Onwuneme <146716349+Onwuneme@users.noreply.github.com>
2025-09-21 13:33:23 +0100

    work done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,10 @@ export default function Navbar() {
         <Link to={"/"}>
           <img src={logo} alt="" className="w-30" />
         </Link>
-        <div className="flex gap-4 md:gap-6 lg:gap-10 text-white font-light text-lg px-5">
-          <Link to={"/Feature"} className={`${location.pathname ==="/Feature"?"border-white":"border-transparent" } border-b-3 py-2`}>Features</Link>
-          <Link to={"/Team"} className={`${location.pathname ==="/Team"?"border-white":"border-transparent" } border-b-3 py-2`}>Team</Link>
-          <button className="py-2">Sign In</button>
+        <div className="flex items-center gap-4 md:gap-6 lg:gap-10 text-white font-light md:text-lg ">
+          <Link to={"/Feature"} className={`${location.pathname ==="/Feature"?"border-white":"border-transparent" } border-b-3 md:py-2`}>Features</Link>
+          <Link to={"/Team"} className={`${location.pathname ==="/Team"?"border-white":"border-transparent" } border-b-3 md:py-2`}>Team</Link>
+          <button className="border-b-3 md:py-2 border-transparent hover:border-white duration-500">Sign In</button>
         </div>
       </div>
     </>
